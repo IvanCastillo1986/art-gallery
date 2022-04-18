a4e921990e4697ed02cb649b4955fe35e1793d5b
Ivan Castillo <total180training@gmail.com>
2022-04-17 23:29:40 -0400

    Added react to project

---
+++ art-gallery-react/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import ArtGallery from './ArtGallery'
+
+function App() {
+  return (
+    <div className="App">
+      <ArtGallery />
+    </div>
+  );
+}
+
+export default App;
