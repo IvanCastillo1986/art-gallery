37361ffd7977d044d304495d47d795fc38db00e6
Ivan Castillo <total180training@gmail.com>
2022-04-30 23:36:25 -0400

    Began writing tests with React Testing Library

--- art-gallery-react/src/App.test.js
+++ art-gallery-react/src/App.test.js
@@ -1,8 +1,19 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
+import Home from './components/Home'
+import CSS from './components/CSS'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+test('render App component', () => {
+  render(<App />, {wrapper: MemoryRouter})
+})
+
+test('it renders the Home page', () => {
+  render(<Home />, {wrapper: MemoryRouter})
+})
+
+test('it renders the CSS page', () => {
+  render(<CSS />)
+})
+
